9867728597826e4209ac0da20df2f1c073ac86d3
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-07-31 22:12:45 +0900

    feat: キーカラーと色調パレットをライブラリ化
    - chroma-js導入
    - Material Design3に習ってColor Systemを導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { tones } = require('./lib/color_system')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,6 +14,87 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        tones: {
+          ...tones
+        },
+        primary: {
+          DEFAULT: tones.P_40,
+          container: tones.P_90,
+        },
+        secondary: {
+          DEFAULT: tones.S_40,
+          container: tones.S_90,
+        },
+        tertiary: {
+          DEFAULT: tones.T_40,
+          container: tones.T_90,
+        },
+        error: {
+          DEFAULT: tones.E_40,
+          container: tones.E_90,
+        },
+        surface: {
+          dim: tones.N_87,
+          DEFAULT: tones.N_98,
+          bright: tones.N_98,
+          container: {
+            lowest: tones.N_100,
+            low: tones.N_96,
+            DEFAULT: tones.N_94,
+            high: tones.N_92,
+            highest: tones.N_90,
+          }
+        },
+        outline: {
+          DEFAULT: tones.NV_50,
+          variant: tones.NV_80,
+        },
+        inverse: {
+          primary: tones.P_80,
+          surface: tones.N_20
+        },
+        background: {
+          DEFAULT: tones.N_98,
+        },
+        shadow: {
+          DEFAULT: tones.N_0,
+        },
+        surface_tint: {
+          DEFAULT: tones.P_40,
+        },
+        scrim: {
+          DEFAULT: tones.N_0,
+        },
+        on: {
+          primary: {
+            DEFAULT: tones.P_100,
+            container: tones.P_10,
+          },
+          secondary: {
+            DEFAULT: tones.S_100,
+            container: tones.S_10,
+          },
+          tertiary: {
+            DEFAULT: tones.T_100,
+            container: tones.T_10,
+          },
+          error: {
+            DEFAULT: tones.E_100,
+            container: tones.E_10,
+          },
+          surface: {
+            DEFAULT: tones.N_10,
+            variant: tones.NV_30,
+          },
+          inverse: {
+            surface: tones.N_95,
+          },
+          background: {
+            DEFAULT: tones.N_10,
+          }
+        }
+      }
     },
   },
   plugins: [],
