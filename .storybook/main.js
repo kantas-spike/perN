328340a79fd03f94403635d7ffe7e73e3f422682
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-08-05 14:52:40 +0900

    env: psudo-statesアドオン追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,7 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "storybook-addon-pseudo-states",
     {
       name: "@storybook/addon-styling",
       options: {
